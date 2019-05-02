265e85ed9521cdb731c96dac07f8db0f89efe7cd
louis11s <louis_schneider@sluz.ch>
2019-05-02 08:19:03 +0200

    Signed-off-by: louis11s <louis_schneider@sluz.ch>

---
+++ Farplan App GUI/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Farplan_App_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Suchen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tBvon = new System.Windows.Forms.TextBox();
+            this.tBnach = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_Abfahrt = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_Switch = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Von = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // btn_Suchen
+            // 
+            this.btn_Suchen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Suchen.Location = new System.Drawing.Point(911, 80);
+            this.btn_Suchen.Name = "btn_Suchen";
+            this.btn_Suchen.Size = new System.Drawing.Size(119, 78);
+            this.btn_Suchen.TabIndex = 0;
+            this.btn_Suchen.Text = "Suchen";
+            this.btn_Suchen.UseVisualStyleBackColor = true;
+            this.btn_Suchen.Click += new System.EventHandler(this.btn_Suchen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Von:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(489, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nach:";
+            // 
+            // tBvon
+            // 
+            this.tBvon.Location = new System.Drawing.Point(98, 80);
+            this.tBvon.Name = "tBvon";
+            this.tBvon.Size = new System.Drawing.Size(287, 26);
+            this.tBvon.TabIndex = 3;
+            this.tBvon.TextChanged += new System.EventHandler(this.tBvon_TextChanged);
+            // 
+            // tBnach
+            // 
+            this.tBnach.Location = new System.Drawing.Point(564, 80);
+            this.tBnach.Name = "tBnach";
+            this.tBnach.Size = new System.Drawing.Size(287, 26);
+            this.tBnach.TabIndex = 4;
+            this.tBnach.TextChanged += new System.EventHandler(this.tBnach_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Datum:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(98, 250);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(315, 26);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(98, 112);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(287, 124);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // btn_Abfahrt
+            // 
+            this.btn_Abfahrt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Abfahrt.Location = new System.Drawing.Point(911, 166);
+            this.btn_Abfahrt.Name = "btn_Abfahrt";
+            this.btn_Abfahrt.Size = new System.Drawing.Size(119, 78);
+            this.btn_Abfahrt.TabIndex = 8;
+            this.btn_Abfahrt.Text = "Abfahrtstafel";
+            this.btn_Abfahrt.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(456, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 37);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Fahrplan";
+            // 
+            // btn_Switch
+            // 
+            this.btn_Switch.Location = new System.Drawing.Point(420, 80);
+            this.btn_Switch.Name = "btn_Switch";
+            this.btn_Switch.Size = new System.Drawing.Size(37, 70);
+            this.btn_Switch.TabIndex = 11;
+            this.btn_Switch.Text = "§";
+            this.btn_Switch.UseVisualStyleBackColor = true;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(564, 112);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(287, 124);
+            this.listBox3.TabIndex = 12;
+            this.listBox3.DoubleClick += new System.EventHandler(this.listBox3_DoubleClick);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Von,
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Location = new System.Drawing.Point(98, 325);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(932, 306);
+            this.listView1.TabIndex = 13;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Von
+            // 
+            this.Von.Text = "Von";
+            this.Von.Width = 100;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Nach";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Zeit";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Abfahrt";
+            this.columnHeader3.Width = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1069, 717);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.btn_Switch);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn_Abfahrt);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tBnach);
+            this.Controls.Add(this.tBvon);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Suchen);
+            this.MinimumSize = new System.Drawing.Size(1091, 773);
+            this.Name = "Form1";
+            this.Text = "Fahrplan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Suchen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tBvon;
+        private System.Windows.Forms.TextBox tBnach;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_Abfahrt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_Switch;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Von;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ Farplan App GUI/Form1.cs
@@ -0,0 +1,98 @@
+﻿using SwissTransport;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Farplan_App_GUI
+{
+    public partial class Form1 : Form
+    {
+        Transport t = new Transport();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+
+        private void btn_Suchen_Click(object sender, EventArgs e)
+        {
+            verbindung(listView1);
+        }
+
+
+        private void btn_Switch_Click(object sender, EventArgs e)
+        {
+            tBvon.Text = tBnach.Text;
+            tBnach.Text = tBvon.Text;
+        }
+
+        private void tBnach_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            listBox3.Items.Clear();
+            Transport t = new Transport();
+            Stations myStations = t.GetStations(tBvon.Text);
+            foreach (Station station in myStations.StationList)
+            {
+                listBox3.Items.Add(station.Name);
+            }
+        }
+
+        private void tBvon_TextChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Transport t = new Transport();
+            Stations myStations = t.GetStations(tBvon.Text);
+            foreach (Station station in myStations.StationList)
+            {
+                listBox1.Items.Add(station.Name);
+            }
+
+        }
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            tBvon.Text = listBox1.SelectedItems[0].ToString();
+            listBox1.Items.Clear();
+        }
+
+        private void tBnach_TextChanged(object sender, EventArgs e)
+        {
+            listBox3.Items.Clear();
+            Transport t = new Transport();
+            Stations myStations = t.GetStations(tBnach.Text);
+            foreach (Station station in myStations.StationList)
+            {
+                listBox3.Items.Add(station.Name);
+            }
+        }
+
+        private void listBox3_DoubleClick(object sender, EventArgs e)
+        {
+            tBnach.Text = listBox3.SelectedItems[0].ToString();
+            listBox3.Items.Clear();
+        }
+        private void verbindung(ListView ListViewName)
+        {
+            Connections Verbindungen = t.GetConnections(tBvon.Text, tBnach.Text);
+
+            foreach (Connection verbindung in Verbindungen.ConnectionList)
+            {
+                ListViewName.Items.Add(verbindung.From.Station.Name);
+                ListViewName.Items.Add(verbindung.To.Station.Name);
+                ListViewName.Items.Add(verbindung.Duration);
+                ListViewName.Items.Add(verbindung.To.Departure);
+            }
+        }
+    }
+}
